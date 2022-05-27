b6cad3fa47e37dcf7dad483f4d48b165f074dfad
Virginie Giacometti <lana.12@wanadoo.fr>
2022-05-27 20:50:49 +0200

    add script=> variable + modif modal

---
+++ initVariable.js
@@ -0,0 +1,45 @@
+/************* Initialisation variable ******************/
+
+// init modal + btn play
+const modalInitPlayer = document.getElementById('modalNamePlayer')
+const modalRules = document.getElementById('modalRules')
+const btnPlay = document.getElementById('play');
+const btnStart = document.getElementById('start');
+const modalWinner = document.getElementById('modalWinner')
+
+//H1 pour l'animation
+const titleAnimation = document.getElementById('titreDiceGame');
+
+// init newGames and Rules
+const newGames = document.getElementById('newGames');
+const rules = document.getElementById('rules');
+
+// initialize the players 
+let namePlayerOne = document.getElementById('userNamePlayer1');//input
+let namePlayerTwo = document.getElementById('userNamePlayer2');//input
+let playerOne = document.getElementById('playerOne');//p
+let playerTwo = document.getElementById('playerTwo');//p
+const input = document.getElementsByClassName('labelPlayer');
+
+//initialize option game
+const rollDice = document.getElementById('rollDice');
+const hold = document.getElementById('hold');
+
+// initialize round players
+let currentPlayerOne = document.getElementById('currentPlayerOne');
+let currentPlayerTwo = document.getElementById('currentPlayerTwo');
+
+//initialize score players
+let scorePlayerOne = document.getElementById('scorePlayerOne');
+let scorePlayerTwo = document.getElementById('scorePlayerTwo');
+
+//initialize current and scoreTotal => pour les deux players
+const currents = document.querySelectorAll('.current');
+const scores = document.querySelectorAll('.scoreTotal');
+
+
+//initialize ActualPlayer + son Style
+const player = document.querySelectorAll('.player');
+
+const color1 = document.querySelector('.player1')
+const color2 = document.querySelector('.player2')
--- script.js
+++ script.js
@@ -1,106 +1,42 @@
 $(document).ready(()=>{
-    console.log('Jquery bien installé')
+    console.log('Jquery bien installé');
     
-    // au chargement page modal regle + background
-    $('#modal').modal('show').css('background','#808080');
+    $('#modalRules').css('background','#808080');
+    $('#modalNamePlayer').css('background','#808080');
 
-    //background modal2
-    let modal2 = $('#modal2');
-    modal2.modal().css('background','#808080');
+    $('h1').hide();
+    $('h1').show(5000);
 
-    // Title display to close modal2
-    $('#start').click(function(){
-        $('h1').hide();
-        $('h1').show(5000);
-    });
-    
-    //Ouverture lien Modal Rules
-    $('#rules').click(function(){
-        $('#modal').modal('show')
-    });
-    //Ouverture lien Modal NewGame
-    $('#newGame').click(function(e){
-        modal2.modal('show')
-    });
-
-});
-/* color blue = #5EBEC4  */
-/* color pink = #FDF5DF   */
-/* background-color: #808080;  */
-
-// init modal + btn play
-const btnPlay = document.getElementById('play');
-const modalInitPlayer = document.getElementById('modal2')
-
-// initialize the players test
-const namePlayerOne = document.getElementById('userNamePlayer1');//input
-const namePlayerTwo = document.getElementById('userNamePlayer2');//input
-const playerOne = document.getElementById('playerOne');//p
-const playerTwo = document.getElementById('playerTwo');//p
-const input = document.getElementsByClassName('labelPlayer');
+}); // fin Jquery
 
 
-/*   Initialize Game*/ // chaque new function à ajouter ds l'ordre
-// const playGame = ()=>{
-//     modalUserName(),
-//     initGame(),
-//     resultDice(),
-//     save()
-
-// };
-
-
-
-//initialize option game
-const rollDice = document.getElementById('rollDice');
-const hold = document.getElementById('hold');
-
-/* function players modal  */
-function addUserName () {
-        playerOne.textContent = namePlayerOne.value[0].toUpperCase() + namePlayerOne.value.substring(1).toLowerCase();
-        playerTwo.textContent = namePlayerTwo.value[0].toUpperCase() + namePlayerTwo.value.substring(1).toLowerCase();
-        
-};
-btnPlay.addEventListener('click', addUserName);
-
+    /* function focus + background input */
 modalInitPlayer.addEventListener('shown.bs.modal', function () {
     namePlayerOne.focus();
     namePlayerOne.style.background='#5EBEC4';
     namePlayerTwo.style.background='#FAD9E6'
+});
+    
+    /* function modal ask names players   */
+function addUserName () {
+    playerOne.textContent = namePlayerOne.value[0].toUpperCase() + namePlayerOne.value.substring(1).toLowerCase();
+    playerTwo.textContent = namePlayerTwo.value[0].toUpperCase() + namePlayerTwo.value.substring(1).toLowerCase();
+};
+    
+btnPlay.addEventListener('click', ()=>{
+    if(namePlayerOne.value == "" && namePlayerTwo.value == ""){
+        namePlayerOne.value = "Player 1"
+        playerOne = namePlayerOne.value
+            
+        namePlayerTwo.value = "Player 2"
+        playerTwo = namePlayerTwo.value
+    }else{
+        addUserName()
+    }
 })
 
-///////////////////////////////////////////////////////////////////
-
-//lier au dom
-const newGames = document.getElementById('newGames');
-// a voir pour au click les regles s'affiche ou id= modal
-const rules = document.getElementById('rules');
-
-// initialize round players
-let currentPlayerOne = document.getElementById('currentPlayerOne');
-let currentPlayerTwo = document.getElementById('currentPlayerTwo');
-
-//initialize score players
-let scorePlayerOne = document.getElementById('scorePlayerOne');
-let scorePlayerTwo = document.getElementById('scorePlayerTwo');
-
-//initialize current and scoreTotal => pour les deux players
-const currents = document.querySelectorAll('.current');
-const scores = document.querySelectorAll('.scoreTotal');
-
-//initialize ActualPlayer + son Style
-const player = document.querySelectorAll('.player');
-const main = document.querySelector('main');
-const color1 = document.querySelector('.player1')
-const color2 = document.querySelector('.player2')
-
 //Function for switch player
-/* color blue = #5EBEC4  */
-/* color pink = #FAD9E6   */
-
 const switchPlayer1 = ()=>{
-    // player[0].setAttribute('class', 'active');
-    // player[1].removeAttribute('class', 'active');
     player[0].style.background='#5EBEC4';
     player[1].style.background='#FAD9E6';
     color1.style.color ='#FAD9E6';
@@ -108,41 +44,38 @@ const switchPlayer1 = ()=>{
     actualPlayer = 0;
 };
 const switchPlayer2 = ()=>{
-    // player[1].setAttribute('class', 'active');
-    // player[0].removeAttribute('class', 'active');
     player[1].style.background='#5EBEC4';
     player[0].style.background= '#FAD9E6';
     color2.style.color = '#FAD9E6';
     color1.style.color ='#5EBEC4';
     actualPlayer = 1;
-    
 };
+
 //initialisation to Game
-// const initGame = () =>{
-    currentPlayerOne = 0;
-    scorePlayerOne = 0;
+
+currentPlayerOne = 0;
+scorePlayerOne = 0;
     
-    currentPlayerTwo = 0;
-    scorePlayerTwo = 0;
+currentPlayerTwo = 0;
+scorePlayerTwo = 0;
     
-    let actualPlayer = 0;
-    let currentPlayer = 0;
-    let diceValue = 0;
+let actualPlayer = 0;
+let currentPlayer = 0;
+let diceValue = 0;
     
-    currents.forEach((current) =>{
+currents.forEach((current) =>{
         current.textContent = 0;
-    });
-    scores.forEach((score) =>{
-        score.textContent = 0;
-    });
-    switchPlayer1(); 
-
-// }
+});
+scores.forEach((score) =>{
+    score.textContent = 0;
+});
+switchPlayer1(); 
 
 
 
-    
-    // a mettre img dé ici + create const ou class img face dé
+// a mettre img dé ici + create const ou class img face dé
+
+
 // nbre aleatoire entre 1 et 6
 const nbDiceRandom = ()=>{
     return Math.floor(Math.random()*6) + 1;  
@@ -150,7 +83,6 @@ const nbDiceRandom = ()=>{
 // Lancer les dés 
 // au clic sur rollDice lance function 
 const resultDice = ()=> {
-    
     diceValue = nbDiceRandom();
     console.log( ' le nombre du dé est '  + diceValue);
     if (diceValue === 1) {
@@ -193,12 +125,9 @@ const save =() =>{
 hold.addEventListener('click', save)
 
 
-// je suis sur la modal
-const textWinner = document.getElementById('textWinner')
-const modalWinner = document.getElementById('modalWinner');
 
 
-
+//A VOIR SI AU CLICK LANCER PLAYGAME
 // reste à faire la modal winner + la fonction
 // faire function initGame + nettoyer le code
 // faire soit en function soit const = () =>
@@ -230,3 +159,4 @@ const modalWinner = document.getElementById('modalWinner');
 // }
 
 // playGame()
+
